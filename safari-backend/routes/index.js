ebe596b2815dd12063745e5c087b75ee0abc2bd8
Rishabhsevensafar <rishabhsevensafar@gmail.com>
2025-02-25 13:10:13 +0530

    Hotel API

--- safari-backend/routes/index.js
+++ safari-backend/routes/index.js
@@ -3,6 +3,7 @@ const adminRoutes = require("./authRoutes");
 const safaribookingRoutes = require("./safaribookingRoutes");
 const paymentRoutes = require("./paymentRoutes");
 const tourpackageRoutes = require("./tourpackageRoutes");
+const hotelRoutes = require("./hotelpackageRoutes");
 
 
 const router = express.Router();
@@ -12,4 +13,5 @@ router.use("/admin", adminRoutes)
 router.use("/booking", safaribookingRoutes)
 router.use("/payment", paymentRoutes)
 router.use("/tourpackage", tourpackageRoutes)
+router.use("/hotel", hotelRoutes)
 module.exports = router;
